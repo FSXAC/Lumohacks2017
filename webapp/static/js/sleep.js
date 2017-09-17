9166272682d29e2036d4332402332055cf20a326
Muchen He <mansurhe@outlook.com>
2017-09-17 08:01:27 -0700

    Database entry

--- webapp/static/js/sleep.js
+++ webapp/static/js/sleep.js
@@ -16,6 +16,7 @@ function View() {
 
 View.prototype.init = function(user) {
     this.$c = $('#main_content_box');
+    this.currentUser;
 
     var homeProp = {
         time: getFormattedTime(),
@@ -34,23 +35,74 @@ View.prototype.init = function(user) {
         $('#s_home-time').html(getFormattedTime());
     }, 60000);
 
-    // Set handler for logging out
-    $('#logoutButton').on('click', function() {
-        
+    // Any JS->HTML bindings
+    $ne_d = $('#newEntry-drag');
+    $ne_d.on('input', function() {
+        $('#newEntry-drag-pre').html($ne_d.val());
     });
+    $ne_dur = $('#newEntry-duration');
+    $ne_dur.on('input', function() {
+        $('#newEntry-duration-pre').html($ne_dur.val());
+    });
+
+    // Adding entry
+    var self = this;
+    $('#saveEntryButton').on('click', function() {
+        // send to server
+        entry = {
+            time: g_date.time(),
+            bedtime: $("#newEntry-time").val(),
+            bedtime_try: $("#newEntry-trime").val(),
+            time_to_fall_asleep: $("#newEntry-drag").val(),
+            interrupts: $("#newEntry-inter").val(),
+            duration: $("#newEntry-duration").val(),
+            time_of_awaken: $("#newEntry-awake").val(),
+            out_of_bed_time: $("#newEntry-outtime").val(),
+            comments: $("#newEntry-comments").val(),
+        }
+
+        $w = $('#newEntryWarning');
+        $wp = $w.find('p');
+        if (entry.bedtime == '' ||
+            entry.bedtime_try == '' ||
+            entry.time_of_awaken == '' ||
+            entry.out_of_bed_time == '') {
+            $w.show();
+            $wp.html('Fill in all the time slots');
+            return;
+        }
+
+        var sleepListRef = firebase.database().ref('users/' + self.currentUser.uid + '/sleep');
+        var newSleepRef = sleepListRef.push();
+        newSleepRef.set(entry);
+
+        // Make modal go away
+        $('#entryModal').modal('hide');
+    });
+
 };
 
 View.prototype.updateUser = function(user) {
-    console.log(user);
+    this.currentUser = user;
 
     // Update name
-    $('#s_name').html(user.displayName.split(' ')[0]);
+    var displayName = user.displayName;
+    if (displayName != null) {
+        $('#s_name').html(displayName.split(' ')[0]);
+    }
 
     // Get UID
-
+    var uid = user.uid;
+    if (uid == null) {
+        console.error("UID is null: abort");
+        return;
+    }
 
     // Access data base
-
+    var userData = firebase.database().ref('users/' + uid);
+    userData.on('value', function(snapshot) {
+        console.log(snapshot);
+    });
 };
 
 View.prototype.setLogoutHandler = function(handler) {
